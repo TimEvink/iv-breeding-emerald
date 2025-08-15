9fcb64f306c1dea8af0cf6ede90259b9f7a036cb
Tim Evink <timevink@gmail.com>
2025-08-16 00:42:22 +0200

    improved table structure and layout

--- src/main.ts
+++ src/main.ts
@@ -19,50 +19,44 @@ const state: State = {
 const stats: readonly string[] = ['HP', 'Atk', 'Def', 'SpA', 'SpD', 'Spe'];
 const tablecellwidth = 8;
 const decimals = 2;
-const tablebody = document.getElementById('tablebody')!;
+const tablebody = document.getElementById('parent-ivs-data')!;
 
 //fetches probability data and constructs the relevant tables.
 function calculateAndRender(): void {
     tablebody.replaceChildren();
     const probabilitydata = probabilityData(state.targetIVs, state.options);
-    //generate tablerows with the data.
     if (probabilitydata.length === 0) {
-        const warningrow = document.createElement('tr');
-        const cell = document.createElement('td');
-        cell.colSpan = tablecellwidth;
-        cell.innerHTML = '<br>No results for current target IVs.<br> Toggle more stats and/or change settings.';
-        warningrow.appendChild(cell);
-        tablebody.appendChild(warningrow);
+        document.getElementById('noresultplaceholder')!.classList.toggle('hidden', false);
         return;
     }
+    document.getElementById('noresultplaceholder')!.classList.toggle('hidden', true);
+    //table data
     for (const [parentAIVs, parentBIVs, numerator, denominator] of probabilitydata) {
         //add a row for each parent
         for (const parent of [parentAIVs, parentBIVs]) {
             const tablerow = document.createElement('tr');
             const parentcell = document.createElement('td');
-            parentcell.classList.add('left-column');
-            parentcell.textContent = (parent === parentAIVs) ? 'Parent A' : 'Parent B';
+            parentcell.textContent = (parent === parentAIVs) ? 'A' : 'B';
             tablerow.appendChild(parentcell);
             for (let i = 0; i < stats.length; i++) {
                 const tabledata = document.createElement('td');
-                tabledata.classList.add('middle-column');
                 tabledata.textContent = parent.includes(i) ? stats[i] : '';
+                // tabledata.classList.add('middle-column');
                 tablerow.appendChild(tabledata);
             }
-            tablebody.appendChild(tablerow);
             //add probabilitycell
             if (parent === parentAIVs) {
                 const probabilitycell = document.createElement('td');
                 probabilitycell.rowSpan = 2;
-                probabilitycell.classList.add('right-column');
                 probabilitycell.textContent = `1/${(denominator / numerator).toFixed(decimals)}`;
                 tablerow.appendChild(probabilitycell);
             }
+            tablebody.appendChild(tablerow);
         }
     }
 }
 
-document.getElementById('target-ivs-header-row')!.addEventListener('click', (event: MouseEvent) => {
+document.getElementById('target-ivs-header-row')!.addEventListener('click', (event) => {
     if (event.target instanceof HTMLTableCellElement && 'stat' in event.target.dataset) {
         //toggle background color.
         event.target.classList.toggle('selected');
