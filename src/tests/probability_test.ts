759918942dc33c7dcc48ac3eda7b9242eec0898d
Tim Evink <timevink@gmail.com>
2025-08-25 21:37:30 +0200

    added some more tests

--- src/tests/probability_test.ts
+++ src/tests/probability_test.ts
@@ -1,32 +1,42 @@
 // @ts-ignore
 import assert from "assert";
 
-import { ivconfigurationGenerator, isRandomGenerationRemainingIVsPossible, countTargetIVsInherited, probability, probabilityData } from '../app/probabilitycalcs.js';
+import { ivconfigurationGenerator, isRandomGenerationRemainingIVsPossible, probability, probabilityData } from '../app/probabilitycalcs.js';
 
 function ivconfigurationGenerator_test() {
     assert.deepStrictEqual([...ivconfigurationGenerator()].length, 960);
 }
 
 function isRandomGenerationRemainingIVsPossible_test() {
-
-}
-
-function countTargetIVsInherited_test() {
-
+    assert.deepStrictEqual(isRandomGenerationRemainingIVsPossible(
+        new Map([[0, 1], [1, 0], [4, 1]]),
+        [0, 4],
+        [1, 4],
+        [0, 1, 4]
+    ), false);
+    assert.deepStrictEqual(isRandomGenerationRemainingIVsPossible(
+        new Map([[0, 1], [1, 0], [4, 1]]),
+        [1, 4],
+        [0, 4],
+        [0, 1, 4]
+    ), true);
+    assert.deepStrictEqual(isRandomGenerationRemainingIVsPossible(
+        new Map([[0, 1], [1, 0], [3, 1]]),
+        [1, 4],
+        [0, 4],
+        [0, 1, 4]
+    ), true);
 }
 
 function probability_test() {
-
-}
-
-function probabilityData_test() {
-
+    assert.deepStrictEqual(probability([0], [0], [0]), [37, 192]);
+    assert.deepStrictEqual(probability([1], [1], [1]), [33, 64]);
+    assert.deepStrictEqual(probability([2], [2], [2]), [17, 48]);
+    assert.deepStrictEqual(probability([0, 2], [2, 5], [0, 2, 5]), [4127, 786432]);
 }
 
 export const probability_test_functions = [
     ivconfigurationGenerator_test,
     isRandomGenerationRemainingIVsPossible_test,
-    countTargetIVsInherited_test,
-    probability_test,
-    probabilityData_test
+    probability_test
 ];
--- src/tests/tests_entry.ts
+++ src/tests/tests_entry.ts
@@ -2,20 +2,20 @@ import { utils_test_functions } from "./utils_test.js";
 import { probability_test_functions } from "./probability_test.js";
 
 function runTest(name: string, func: () => void) {
-  try {
-    func();
-    console.log(`✅ ${name}`);
-  } catch (err) {
-    console.error(`❌ ${name}`);
-    console.error(err);
-  }
+	try {
+		func();
+		console.log(`✅ ${name}`);
+	} catch (err) {
+		console.error(`❌ ${name}`);
+		console.error(err);
+	}
 }
 
 //gather tests.
 const tests = [
-  ...utils_test_functions,
-  ...probability_test_functions
-];
+    ...utils_test_functions,
+    ...probability_test_functions
+]
 
 //run tests.
 for (const func of tests) {
