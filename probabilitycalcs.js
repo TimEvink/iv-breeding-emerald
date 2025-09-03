c5104400f6d0c5a5ce52cf01c36ae2f0e7607ac4
TimEvink <TimEvink@users.noreply.github.com>
2025-09-03 12:51:46 +0000

    deploy: 6651e946994f6587ec224ee76d1ccef46426318f

--- probabilitycalcs.js
+++ probabilitycalcs.js
@@ -1,8 +1,18 @@
 import { gcd, combinations, lruCache } from './utils.js';
+//stats are worked internally with by integers: we use 0, 1, 2, 3, 4, 5 for HP, Atk, Def, Sp.A, Sp.D and Spe, respectively.
+//also: 0 is used for parent A, 1 is used for parent B.
+// example: [[0, 0], [4, 1], [5, 0]] means in the inheritance process we have in order:
+// - HP is passed from parent A
+// - Sp.D is passed from parent B
+// - SpE is passed from A
+//
+// the order is important as newer steps can override previously inherited stats.
 export function* ivconfigurationGenerator() {
     for (let s1 = 0; s1 < 6; s1++) {
-        for (const s2 of [1, 2, 3, 4, 5]) {
-            for (const s3 of [1, 3, 4, 5]) {
+        for (let s2 = 1; s2 < 6; s2++) {
+            for (let s3 = 1; s3 < 6; s3++) {
+                if (s3 === 2)
+                    continue;
                 for (let p1 = 0; p1 < 2; p1++) {
                     for (let p2 = 0; p2 < 2; p2++) {
                         for (let p3 = 0; p3 < 2; p3++) {
@@ -81,5 +91,6 @@ function probabilityData(targetIVs, options) {
 //probabilityData wrapped to add caching.
 export const probabilityDataWithCache = lruCache(probabilityData, {
     maxSize: 20,
-    shouldCache: (data) => data.length > 3
+    shouldCache: (data) => data.length > 3,
+    makeKey: (targetIVs, options) => `${options.missingAIVs}${options.missingBIVs}${targetIVs.join('')}`
 });
